3e23ba5e292eba8b62b8c77d4053eac492c1b125
plugatarev <plugatarevk@gmail.com>
2022-04-07 15:13:27 +0700

    Converted class Token to a record

--- src/main/java/com/github/plugatarev/bencode/Lexer/Token.java
+++ src/main/java/com/github/plugatarev/bencode/Lexer/Token.java
@@ -2,34 +2,7 @@ package com.github.plugatarev.bencode.Lexer;
 
 import java.util.Objects;
 
-public final class Token {
-    private final TokenType tokenType;
-    private final int nLine;
-    private final int pos;
-    private final Object value;
-
-    public Token(TokenType tokenType, int nLine, int pos, Object value) {
-        this.tokenType = tokenType;
-        this.nLine = nLine;
-        this.pos = pos;
-        this.value = value;
-    }
-
-    public TokenType tokenType() {
-        return tokenType;
-    }
-
-    public int nLine() {
-        return nLine;
-    }
-
-    public int pos() {
-        return pos;
-    }
-
-    public Object value() {
-        return value;
-    }
+public record Token(TokenType tokenType, int nLine, int pos, Object value) {
 
     @Override
     public boolean equals(Object obj) {
