7635695a3e34e3f4fd914db5c2ef6a2c62260c0d
plugatarev <plugatarevk@gmail.com>
2022-04-10 13:53:21 +0700

    Convert Emelents to Bencode format

--- src/main/java/com/github/plugatarev/bencode/parser/Element.java
+++ src/main/java/com/github/plugatarev/bencode/parser/Element.java
@@ -5,39 +5,13 @@ import java.util.Map;
 
 public sealed interface Element {
 
-    record JInteger(int value) implements Element {
-        @Override
-        public String toString() {
-            return String.valueOf(value);
-        }
-    }
+    record BInteger(int value) implements Element {}
 
-    record JArray(List<Element> member) implements Element{
-        @Override
-        public String toString(){
-            return member.toString();
-        }
-    }
+    record BArray(List<Element> member) implements Element{}
 
-    record JDictionary(Map<Element, Element> dict, int nestingLevel) implements Element{
-        @Override
-        public String toString(){
-            StringBuilder str = new StringBuilder("\n%s{\n".formatted("\t".repeat(nestingLevel - 1)));
-            for (Map.Entry<Element, Element> entry : dict.entrySet()) {
-                str.append("%s%s: %s\n".formatted("\t".repeat(nestingLevel), entry.getKey().toString(), entry.getValue().toString()));
-            }
-            str.append("%s}\n".formatted("\t".repeat(nestingLevel - 1)));
-            return str.toString();
+    record BDictionary(Map<Element, Element> dict, int nestingLevel) implements Element{}
 
-        }
-    }
-
-    record JString(String str) implements  Element{
-        @Override
-        public String toString(){
-            return "\"" + str + "\"";
-        }
-    }
+    record BString(String str) implements  Element{}
 
 }
 
