f9cd9d87d7db438f6c4996179e521e91eb2a9e62
plugatarev <plugatarevk@gmail.com>
2022-04-10 13:54:45 +0700

    converter from bencode to json

--- src/main/java/com/github/plugatarev/bencode/JsonConverter.java
+++ src/main/java/com/github/plugatarev/bencode/JsonConverter.java
@@ -2,27 +2,41 @@ package com.github.plugatarev.bencode;
 
 import com.github.plugatarev.bencode.parser.Element;
 
-//public class JsonConverter {
-//
-//    int nestingLevel;
-//    private static int SHIFT = 2;
-//
-//    public String json(Element element) {
-//        return switch (element) {
-//            case Element.JInteger i -> shifted(i.value());
-//            case Element.JDictionary jDictionary -> {
-//                nestingLevel++;
-//                // for loop
-//                nestingLevel--;
-//            }
-//            case Element.JArray jArray -> {
-//            }
-//            case Element.JString jString -> {
-//            }
-//        };
-//    }
-//
-//    private String shifted(Object value) {
-//        return " ".repeat(nestingLevel * SHIFT) + value;
-//    }
-//}
+import java.util.Map;
+
+public class JsonConverter {
+
+    int nestingLevel;
+    private static final int SHIFT = 2;
+
+    public String json(Element element) {
+        return switch (element) {
+            case Element.BInteger jInteger -> shifted(jInteger.value());
+            case Element.BDictionary jDictionary -> {
+                StringBuilder str = new StringBuilder();
+                if (nestingLevel == 0) appendStrings(str, shifted("{"), "\n");
+                else appendStrings(str, "\n", shifted("{"), "\n");
+
+                nestingLevel++;
+                for (Map.Entry<Element, Element> entry : jDictionary.dict().entrySet()) {
+                    appendStrings(str, shifted(entry.getKey()), ": ", json(entry.getValue()), "\n");
+                }
+                nestingLevel--;
+                str.append(shifted("}"));
+                yield str.toString();
+            }
+            case Element.BArray jArray -> jArray.member().toString();
+            case Element.BString jString -> jString.toString();
+        };
+    }
+
+    private void appendStrings(StringBuilder string, String... part){
+        for (String p : part){
+            string.append(p);
+        }
+    }
+
+    private String shifted(Object value) {
+        return " ".repeat(nestingLevel * SHIFT) + value;
+    }
+}
