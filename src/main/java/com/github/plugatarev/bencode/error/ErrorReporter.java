39159b69ad742297b935efdc6299e96c894957b8
plugatarev <plugatarevk@gmail.com>
2022-04-22 15:03:53 +0700

    Removed Empty retorter

--- src/main/java/com/github/plugatarev/bencode/error/ErrorReporter.java
+++ src/main/java/com/github/plugatarev/bencode/error/ErrorReporter.java
@@ -1,7 +1,5 @@
 package com.github.plugatarev.bencode.error;
 public interface ErrorReporter {
-    ErrorReporter EMPTY = message -> true;
-
     /**
      * @param message error message
      * @return false if it no more errors can be reported
