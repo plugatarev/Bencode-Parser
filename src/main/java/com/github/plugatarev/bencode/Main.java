85fe2d21515cc48bafacd205b22301cbff6a312b
plugatarev <plugatarevk@gmail.com>
2022-04-23 07:44:44 +0700

    Example

--- src/main/java/com/github/plugatarev/bencode/Main.java
+++ src/main/java/com/github/plugatarev/bencode/Main.java
@@ -14,14 +14,14 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        String expressions = "d3:cow3:moo3:keyd2:bb3:hhh2:ffd4:keysl1:i1:d1:l1:eeee4:spaml4:infoi32eee";
+        String expressions = "d3:cow3:moo3:keyd2:bb3:hhh2:ffd4:keysl1:i1:d1:l1:eeee4:spaml4:infoi343242e5:6$%!$ee";
         Reader reader = new StringReader(expressions);
         BufferedReader br = new BufferedReader(reader);
         ErrorReporter reporter = new ConsoleReporter();
-
         List<Token> tokens = Lexer.scan(br, reporter);
-        Element jsonTokens = Parser.parse(tokens, new ConsoleReporter());
+        if (tokens == null) return;
+        Element bTokens = Parser.parse(tokens, new ConsoleReporter());
         JsonConverter converter = new JsonConverter();
-        System.out.println(converter.json(jsonTokens));
+        System.out.println(converter.json(bTokens));
     }
 }
